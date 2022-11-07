d5637e2f17d3d5cf91074acaa4350ad1df216118
imranHRafi <senbagiimran@gmail.com>
2022-11-07 13:57:22 +0600

    added vite font configuaration

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { ViteWebfontDownload } from 'vite-plugin-webfont-dl';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [
+    react(),
+    ViteWebfontDownload([
+      'https://fonts.googleapis.com/css2?family=Libre+Baskerville:wght@700&family=Source+Sans+Pro:wght@400;600;900&display=swap',
+    ]),
+  ],
+});
